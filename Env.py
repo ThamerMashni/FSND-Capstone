8043ab25651cce48c17893ed248e22b0d38d5c25
ThamerMashni <Thamer.asiri@hotmail.com>
2021-01-28 13:06:30 +0300

    commit

--- Env.py
+++ Env.py
@@ -3,7 +3,7 @@ AUTH0_DOMAIN="mashni.eu.auth0.com"
 ALGORITHMS="RS256"
 API_AUDIENCE="agency"
 AUTH0_CLIENT_ID="vcylBDMoFwrLB19THy7t7MlMEY4JJkXS"
-AUTH0_CALLBACK_URL="http://localhost:8080"
+AUTH0_CALLBACK_URL="https://fsnd-capstone-mashni.herokuapp.com/"
 
 #DB
 DB_NAME="capstone"
