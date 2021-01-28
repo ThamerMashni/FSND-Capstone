1aec33f796280e4b40bd90b0a46ff1fe59af2bcf
ThamerMashni <Thamer.asiri@hotmail.com>
2021-01-28 13:00:48 +0300

    commit

--- app.py
+++ app.py
@@ -228,5 +228,5 @@ def create_app(test_config=None):
 APP = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = process.env.PORT
     APP.run(host='0.0.0.0', port=port, debug=True)
