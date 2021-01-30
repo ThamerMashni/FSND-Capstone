72de22b15f2ed971530c817beb3177ffef0a3c9e
ThamerMashni <Thamer.asiri@hotmail.com>
2021-01-30 15:49:04 +0300

    db url

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from flask_migrate import Migrate
 from Env import DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME, DB_HOST
 
 # database_path = 'postgres://'f'{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
 db = SQLAlchemy()
 
 
